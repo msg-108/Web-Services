3874c64c9f0c2da652eec3be46d22a5f491d30eb
Madhusudhan Gharti <madhusudhangharti.108@gmail.com>
2026-06-07 09:07:38 +0545

    add GuessingGame

--- java/GuessingGame.java
+++ java/GuessingGame.java
@@ -1,5 +1,34 @@
-class GuessingGaame{
+import java.util.Random;
+import java.util.Scanner;
+
+class GuessingGame{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("Welcome to the Guessing Game!"); 
+
+        // Generate a random number between 1 and 100 
+        Random rand = new Random();
+        int target = rand.nextInt(100) + 1;
+
+        // Let user guess the number 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guess a number between 1 and 100:");
+        int guess = scanner.nextInt();
+
+        // if guess < target -> "Your guess is lower than target, Guess higher"
+        if (guess < target) {
+            System.out.println("Your guess is lower than target, Guess higher");
+        }
+
+        // if guess > target -> "Your guess is higher than target, Guess lower"
+        else if (guess > target) {
+            System.out.println("Your guess is higher than target, Guess lower");
+        }
+
+        // if guess == target -> "Congratulations! You guessed the number."
+        else {
+            System.out.println("Congratulations! You guessed the number.");
+        }
+
     }
 }
