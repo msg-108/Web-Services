b8d546f46cc6344f58b423bc883a225c1c009a42
Madhusudhan Gharti <madhusudhangharti.108@gmail.com>
2026-06-08 15:16:38 +0545

    change exit mechanism

--- java/GuessingGame.java
+++ java/GuessingGame.java
@@ -20,38 +20,36 @@ class GuessingGame{
 
             // Let user guess the number 
             System.out.println("\nGuess a number between 1 and 100:");
-            int guess = scanner.nextInt();
+            System.out.println("(Enter any character to exit.)");
+            
+            if(scanner.hasNextInt()){
+                int guess = scanner.nextInt();
 
-            // Handling invalid inputs
-            if (guess <= 0 || guess > 100){
-                System.out.println("ERROR: Invalid input!");
-                continue;
-            }
+                // Handling invalid inputs
+                if (guess <= 0 || guess > 100){
+                    System.out.println("ERROR: Invalid input!");
+                    continue;
+                }
 
-            ++count;
+                ++count;
 
-            // if guess < target -> "Your guess is lower than target, Guess higher"
-            if (guess < target) {
-                System.out.println("\nYour guess is lower than target, Guess higher");
-            }
+                // if guess < target -> "Your guess is lower than target, Guess higher"
+                if (guess < target) {
+                    System.out.println("\nYour guess is lower than target, Guess higher");
+                }
 
-            // if guess > target -> "Your guess is higher than target, Guess lower"
-            else if (guess > target) {
-                System.out.println("\nYour guess is higher than target, Guess lower");
-            }
+                // if guess > target -> "Your guess is higher than target, Guess lower"
+                else if (guess > target) {
+                    System.out.println("\nYour guess is higher than target, Guess lower");
+                }
 
-            // if guess == target -> "Congratulations! You guessed the number."
-            else {
-                System.out.println("\nCongratulations! You guessed the number.");
-                System.out.println("\nYou have guessed correct answer in " + count + " attempts.");
-                break;
-            }
-
-            // Ask user if they want to try again
-            System.out.println("\nDo you want to try again?(Y/Yes to continue)");
-            String again = scanner.next().trim().toLowerCase();
-
-            if (!again.equals("yes") && !again.equals("y")) {
+                // if guess == target -> "Congratulations! You guessed the number."
+                else {
+                    System.out.println("\nCongratulations! You guessed the number.");
+                    System.out.println("\nYou have guessed correct answer in " + count + " attempts.");
+                    break;
+                }
+            } else{
                 break;
             }
 
