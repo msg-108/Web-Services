2a1655ee31ad07ac3929808fe31fa51361e6bea9
Madhusudhan Gharti <246828956+msg-108@users.noreply.github.com>
2026-06-13 08:59:28 +0545

    feat: take contact's input

--- ContactsBook/ContactsBook.java
+++ ContactsBook/ContactsBook.java
@@ -1,7 +1,28 @@
+
+import java.util.Scanner;
+
 public class ContactsBook{
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Contacts Book!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        // take contact name
+        System.out.println("Enter contact's name: ");
+        String contactName = scanner.nextLine();
+
+        // take contact number
+        System.out.println("Enter " + contactName + "'s number: ");
+        String contactNumber = scanner.next();
+        
+        // take address
+        System.out.println("Enter " + contactName + "'s address: ");
+        String contactAddress = scanner.nextLine();
+
+        // take email
+        System.out.println("Enter " + contactName + "'s email: ");
+        String contactEmail = scanner.next();
         
     }
 }
