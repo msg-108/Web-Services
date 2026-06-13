4d25214cbd76b9d21fe18f01c3afe03c7adf2bcd
Madhusudhan Gharti <246828956+msg-108@users.noreply.github.com>
2026-06-13 09:28:46 +0545

    feat: store input data on contact class

--- ContactsBook/ContactsBook.java
+++ ContactsBook/ContactsBook.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 
 public class ContactsBook{
 
+    private static class Contact {
+        String contactName;
+        String contactNumber;
+        String contactAddress;
+        String contactEmail;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Contacts Book!");
         
         Scanner scanner = new Scanner(System.in);
-        
+
         // take contact name
         System.out.println("Enter contact's name: ");
         String contactName = scanner.nextLine();
@@ -15,7 +22,7 @@ public class ContactsBook{
         // take contact number
         System.out.println("Enter " + contactName + "'s number: ");
         String contactNumber = scanner.nextLine();
-        
+
         // take address
         System.out.println("Enter " + contactName + "'s address: ");
         String contactAddress = scanner.nextLine();
@@ -24,12 +31,19 @@ public class ContactsBook{
         System.out.println("Enter " + contactName + "'s email: ");
         String contactEmail = scanner.nextLine();
         
-
+        // create contact object
+        Contact contact = new Contact();
+        contact.contactName = contactName;
+        contact.contactNumber = contactNumber;
+        contact.contactAddress = contactAddress;
+        contact.contactEmail = contactEmail;
+        
         // print contact details
         System.out.println("\nContact Details:");
-        System.out.println("Name: " + contactName);
-        System.out.println("Number: " + contactNumber);
-        System.out.println("Address: " + contactAddress);
-        System.out.println("Email: " + contactEmail);
+        System.out.println("Name: " + contact.contactName);
+        System.out.println("Number: " + contact.contactNumber);
+        System.out.println("Address: " + contact.contactAddress);
+        System.out.println("Email: " + contact.contactEmail);
+        
     }
 }
